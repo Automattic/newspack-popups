7dc67df8f070795d7007830de09d373906f44d7e
Jefferson Rabb <jeff@atavist.net>
2019-10-26 16:32:25 -0400

    Plugin scaffolding.

---
+++ newspack-popups.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Newspack Popups
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     newspack-popups
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Newspack_Popups
+ */
+
+// Your code starts here.
