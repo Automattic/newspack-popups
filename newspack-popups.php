67e0aa5a53def2222a161fe3b1982af3600fde1f
claudiulodro <claudiulodro@gmail.com>
2019-12-10 10:38:50 -0800

    Bump version for release

--- newspack-popups.php
+++ newspack-popups.php
@@ -7,7 +7,7 @@
  * Author URI:      https://newspack.block
  * Text Domain:     newspack-popups
  * Domain Path:     /languages
- * Version:         0.1.0
+ * Version:         1.0.0
  *
  * @package         Newspack_Popups
  */
