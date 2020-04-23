0dd559304f0cc24bab07022b3a6edaead08c2549
Adam Borowski <adam@adamboro.com>
2020-04-23 13:03:43 +0200

    feat: enable disabling popups for specific posts and pages
    - remove assess_amp_form_issue

--- webpack.config.js
+++ webpack.config.js
@@ -6,27 +6,23 @@
 /**
  * External dependencies
  */
-const fs = require( 'fs' );
 const getBaseWebpackConfig = require( '@automattic/calypso-build/webpack.config.js' );
 const path = require( 'path' );
-
-/**
- * Internal dependencies
- */
-// const { workerCount } = require( './webpack.common' ); // todo: shard...
 
 /**
  * Internal variables
  */
 const editor = path.join( __dirname, 'src', 'editor' );
 const view = path.join( __dirname, 'src', 'view' );
+const documentSettings = path.join( __dirname, 'src', 'document-settings' );
 
 const webpackConfig = getBaseWebpackConfig(
 	{ WP: true },
 	{
 		entry: {
-			editor: editor,
-			view: view,
+			editor,
+			view,
+			documentSettings,
 		},
 		'output-path': path.join( __dirname, 'dist' ),
 	}
