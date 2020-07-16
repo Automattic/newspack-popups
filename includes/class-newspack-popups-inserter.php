81e01fceec66fa3d83c0d6b4559bc85ffb5b45f9
Adam Borowski <adam@adamboro.com>
2020-07-16 16:13:12 +0200

    fix: js error on non-AMP pages

--- includes/class-newspack-popups-inserter.php
+++ includes/class-newspack-popups-inserter.php
@@ -271,7 +271,7 @@ final class Newspack_Popups_Inserter {
 			wp_register_script(
 				'newspack-popups-view',
 				plugins_url( '../dist/view.js', __FILE__ ),
-				null,
+				[ 'wp-dom-ready' ],
 				filemtime( dirname( NEWSPACK_POPUPS_PLUGIN_FILE ) . '/dist/view.js' ),
 				true
 			);
