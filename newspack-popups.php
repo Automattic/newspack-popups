62ac121e7fc465e136bab3b2b31ff350165b2d46
matticbot <semantic-release-bot@martynus.net>
2020-09-15 15:50:14 +0000

    chore(release): 1.11.0-alpha.1 [skip ci]
    # [1.11.0-alpha.1](https://github.com/Automattic/newspack-popups/compare/v1.10.0...v1.11.0-alpha.1) (2020-09-15)
    
    ### Bug Fixes
    
    * handle false get_transient return value ([#211](https://github.com/Automattic/newspack-popups/issues/211)) ([11c9f3b](https://github.com/Automattic/newspack-popups/commit/11c9f3b17bcc125e9a87dfce942f03194e9c5657))
    
    ### Features
    
    * lightweight GET API for campaigns ([#208](https://github.com/Automattic/newspack-popups/issues/208)) ([3bae873](https://github.com/Automattic/newspack-popups/commit/3bae8733a17e58db779c595ea2c2c82c9beb92f4))

--- newspack-popups.php
+++ newspack-popups.php
@@ -7,7 +7,7 @@
  * Author URI:      https://newspack.blog
  * Text Domain:     newspack-popups
  * Domain Path:     /languages
- * Version:         1.10.0
+ * Version:         1.11.0-alpha.1
  *
  * @package         Newspack_Popups
  */
