3816b28e252c632b294de7ee9dc814a01bb699b0
Adam Borowski <adam@adamboro.com>
2020-09-17 13:41:25 +0200

    feat: you've got one shot, one opportunity

--- includes/class-newspack-popups.php
+++ includes/class-newspack-popups.php
@@ -396,6 +396,10 @@ final class Newspack_Popups {
 	 * Create the config file for the API, unless it exists.
 	 */
 	public static function create_lightweight_api_config() {
+		if ( get_option( 'newspack_has_tried_to_create_lightweight_api_config' ) ) {
+			return;
+		};
+		add_option( 'newspack_has_tried_to_create_lightweight_api_config', true );
 		$has_db_config_in_env = getenv( 'DB_USER' ) && getenv( 'DB_PASSWORD' ) && getenv( 'DB_NAME' ) && getenv( 'DB_HOST' );
 		if ( ! $has_db_config_in_env || file_exists( self::LIGHTWEIGHT_API_CONFIG_FILE_PATH ) ) {
 			return;
