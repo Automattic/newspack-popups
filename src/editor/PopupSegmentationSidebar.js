e5447eb589165ca6f4802fe638578ca14b1ec4cd
Derrick Koo <derrick.koo@automattic.com>
2020-11-11 14:42:06 -0700

    fix: add missing untracked file

---
+++ src/editor/PopupSegmentationSidebar.js
@@ -0,0 +1,58 @@
+/**
+ * Popup frequency options.
+ */
+
+/**
+ * WordPress dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { Component, Fragment } from '@wordpress/element';
+import { ExternalLink, SelectControl } from '@wordpress/components';
+
+const segmentsList =
+	( window && window.newspack_popups_data && window.newspack_popups_data.segments ) || [];
+
+class PopupSegmentationSidebar extends Component {
+	/**
+	 * Render
+	 */
+	render() {
+		const { onMetaFieldChange, selected_segment_id } = this.props;
+
+		return (
+			<Fragment>
+				<SelectControl
+					label={ __( 'Segment' ) }
+					help={
+						! selected_segment_id
+							? __( 'The campaign will be shown to all readers.', 'newspack-popups' )
+							: __(
+									'The campaign will be shown only to readers who match the selected segment.',
+									'newspack-popups'
+							  )
+					}
+					value={ selected_segment_id }
+					onChange={ value => onMetaFieldChange( 'selected_segment_id', value ) }
+					options={ [
+						{
+							value: '',
+							label: __( 'All readers', 'newspack-popups' ),
+						},
+						...segmentsList.map( segment => ( {
+							value: segment.id,
+							label: segment.name,
+						} ) ),
+					] }
+				/>
+				<ExternalLink
+					href="/wp-admin/admin.php?page=newspack-popups-wizard#/segmentation"
+					key="segmentation-link"
+				>
+					{ __( 'Manage your campaign segments' ) }
+				</ExternalLink>
+			</Fragment>
+		);
+	}
+}
+
+export default PopupSegmentationSidebar;
