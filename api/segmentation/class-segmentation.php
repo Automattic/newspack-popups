326fb103d0eb9f05321c5598eac47ced378dcc34
Adam Borowski <adam@adamboro.com>
2020-12-07 14:47:31 +0100

    feat: move custom GA config endpoint to lightweight API

--- api/segmentation/class-segmentation.php
+++ api/segmentation/class-segmentation.php
@@ -12,6 +12,13 @@ defined( 'ABSPATH' ) || exit;
  */
 class Segmentation {
 	/**
+	 * Names of custom dimensions options.
+	 */
+	const CUSTOM_DIMENSIONS_OPTION_NAME_READER_FREQUENCY = 'newspack_popups_cd_reader_frequency';
+	const CUSTOM_DIMENSIONS_OPTION_NAME_IS_SUBSCRIBER    = 'newspack_popups_cd_is_subscriber';
+	const CUSTOM_DIMENSIONS_OPTION_NAME_IS_DONOR         = 'newspack_popups_cd_is_donor';
+
+	/**
 	 * Get log file path.
 	 */
 	public static function get_log_file_path() {
