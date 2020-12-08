1c77ea5afdbf3e9e0aeb6ffe18a0307614d85fff
Derrick Koo <derrick.koo@automattic.com>
2020-12-07 18:56:54 -0700

    fix: only render preview after last block

--- src/editor/withDismissButtonPreview.js
+++ src/editor/withDismissButtonPreview.js
@@ -6,19 +6,17 @@ import { createHigherOrderComponent } from '@wordpress/compose';
 
 const withDismissButtonPreview = createHigherOrderComponent( BlockListBlock => {
 	return props => {
-		const { rootClientId } = props;
-		const meta = useSelect( select => {
-			const { getEditedPostAttribute } = select( 'core/editor' );
-			return getEditedPostAttribute( 'meta' );
-		} );
-
+		const { index, rootClientId } = props;
+		const meta = useSelect( select => select( 'core/editor' ).getEditedPostAttribute( 'meta' ) );
+		const blockCount = useSelect( select => select( 'core/block-editor' ).getBlockCount() );
 		const { dismiss_text, dismiss_text_alignment } = meta;
 		const alignClass = 'has-text-align-' + ( dismiss_text_alignment || 'center' );
+		const isLastBlock = index === blockCount - 1;
 
 		return (
 			<>
 				<BlockListBlock { ...props } />
-				{ dismiss_text && ! rootClientId && (
+				{ dismiss_text && ! rootClientId && isLastBlock && (
 					<div
 						className={ `newspack-popups__not-interested-button-preview wp-block ${ alignClass }` }
 					>
