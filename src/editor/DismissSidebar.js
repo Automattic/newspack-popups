8c6c2bb37ac45da070d0601841456e2d4a9f2e04
Derrick Koo <derrick.koo@automattic.com>
2020-12-09 09:47:41 -0700

    fix: render preview even when Dismiss Button Settings sidebar is closed

--- src/editor/DismissSidebar.js
+++ src/editor/DismissSidebar.js
@@ -6,36 +6,10 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { Fragment, useEffect } from '@wordpress/element';
+import { Fragment } from '@wordpress/element';
 import { SelectControl, TextControl } from '@wordpress/components';
 
 const DismissSidebar = ( { dismiss_text, dismiss_text_alignment, onMetaFieldChange } ) => {
-	// Render a preview of the dismiss button at the end of the block content area.
-	useEffect(() => {
-		const alignClass = 'has-text-align-' + ( dismiss_text_alignment || 'center' );
-
-		let dismissButtonPreview = document.querySelector(
-			'.newspack-popups__not-interested-button-preview'
-		);
-
-		if ( ! dismissButtonPreview ) {
-			const rootContainer = document.querySelector(
-				'.block-editor-block-list__layout.is-root-container'
-			);
-
-			if ( rootContainer ) {
-				dismissButtonPreview = document.createElement( 'div' );
-				rootContainer.appendChild( dismissButtonPreview );
-			}
-		}
-
-		if ( dismissButtonPreview ) {
-			dismissButtonPreview.className =
-				'newspack-popups__not-interested-button-preview wp-block ' + alignClass;
-			dismissButtonPreview.textContent = dismiss_text;
-		}
-	}, [ dismiss_text, dismiss_text_alignment ]);
-
 	return (
 		<Fragment>
 			<TextControl
--- src/editor/index.js
+++ src/editor/index.js
@@ -7,7 +7,8 @@
  */
 import { __ } from '@wordpress/i18n';
 import { compose } from '@wordpress/compose';
-import { withSelect, withDispatch } from '@wordpress/data';
+import { useSelect, withSelect, withDispatch } from '@wordpress/data';
+import { useEffect } from '@wordpress/element';
 import { registerPlugin } from '@wordpress/plugins';
 import { PluginDocumentSettingPanel, PluginPostStatusInfo } from '@wordpress/edit-post';
 
@@ -110,14 +111,49 @@ registerPlugin( 'newspack-popups-segmentation', {
 } );
 
 registerPlugin( 'newspack-popups-dismiss', {
-	render: () => (
-		<PluginDocumentSettingPanel
-			name="popup-dismiss-panel"
-			title={ __( 'Dismiss Button Settings', 'newspack-popups' ) }
-		>
-			<DismissSidebarWithData />
-		</PluginDocumentSettingPanel>
-	),
+	render: () => {
+		const meta = useSelect( select => select( 'core/editor' ).getEditedPostAttribute( 'meta' ) );
+		const { dismiss_text, dismiss_text_alignment } = meta;
+
+		// Render a preview of the dismiss button at the end of the block content area.
+		useEffect(() => {
+			if ( ! dismiss_text ) {
+				return;
+			}
+
+			const alignClass = 'has-text-align-' + ( dismiss_text_alignment || 'center' );
+
+			let dismissButtonPreview = document.querySelector(
+				'.newspack-popups__not-interested-button-preview'
+			);
+
+			if ( ! dismissButtonPreview ) {
+				const rootContainer = document.querySelector(
+					'.block-editor-block-list__layout.is-root-container'
+				);
+
+				if ( rootContainer ) {
+					dismissButtonPreview = document.createElement( 'div' );
+					rootContainer.appendChild( dismissButtonPreview );
+				}
+			}
+
+			if ( dismissButtonPreview ) {
+				dismissButtonPreview.className =
+					'newspack-popups__not-interested-button-preview wp-block ' + alignClass;
+				dismissButtonPreview.textContent = dismiss_text;
+			}
+		}, [ dismiss_text, dismiss_text_alignment ]);
+
+		return (
+			<PluginDocumentSettingPanel
+				name="popup-dismiss-panel"
+				title={ __( 'Dismiss Button Settings', 'newspack-popups' ) }
+			>
+				<DismissSidebarWithData />
+			</PluginDocumentSettingPanel>
+		);
+	},
 	icon: null,
 } );
 
