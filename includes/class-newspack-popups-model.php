218cd5fb12cd46dfe84142ed936c928b539df096
Jefferson Rabb <jeff@atavist.net>
2021-01-29 12:26:27 -0500

    fix: unset unknown segment ids on prompts

--- includes/class-newspack-popups-model.php
+++ includes/class-newspack-popups-model.php
@@ -440,6 +440,13 @@ final class Newspack_Popups_Model {
 				]
 			),
 		];
+
+		if (
+			$popup['options']['selected_segment_id'] && ! in_array( Newspack_Popups_Segmentation::get_segment_ids(), $popup['options']['selected_segment_id'] ) ) {
+			$popup['options']['selected_segment_id'] = null;
+		}
+
+
 		if ( $include_categories ) {
 			$popup['categories']      = get_the_category( $id );
 			$popup['campaign_groups'] = get_the_terms( $id, Newspack_Popups::NEWSPACK_POPUPS_TAXONOMY );
