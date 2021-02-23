68e17eea77d0cf46202dd607ec99be540289a011
Derrick Koo <derrick.koo@automattic.com>
2021-02-23 15:41:46 -0700

    fix: when previewing, check all segments assigned to each prompt

--- api/campaigns/class-maybe-show-campaign.php
+++ api/campaigns/class-maybe-show-campaign.php
@@ -306,7 +306,7 @@ class Maybe_Show_Campaign extends Lightweight_API {
 			$should_display = false;
 			if ( isset( $view_as_spec['segment'] ) && $view_as_spec['segment'] ) {
 				// Show prompts with a matching segment, or "everyone". Don't show any prompts that don't match the previewed segment.
-				if ( $view_as_spec['segment'] === $campaign->s || empty( $campaign->s ) ) {
+				if ( in_array( $view_as_spec['segment'], $campaign_segment_ids ) || empty( $campaign->s ) ) {
 					$should_display = true;
 				}
 
