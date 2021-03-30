dba319bb8b56eba5a4e9c4b0b073f7676e487823
matticbot <semantic-release-bot@martynus.net>
2021-03-30 22:47:50 +0000

    chore(release): 1.29.0 [skip ci]
    # [1.29.0](https://github.com/Automattic/newspack-popups/compare/v1.28.0...v1.29.0) (2021-03-30)
    
    ### Features
    
    * use RadioControls for prompt type selection ([#501](https://github.com/Automattic/newspack-popups/issues/501)) ([ebb6776](https://github.com/Automattic/newspack-popups/commit/ebb6776788d0a9cda4bad92349d28fe9cbeefb4f))

--- newspack-popups.php
+++ newspack-popups.php
@@ -7,7 +7,7 @@
  * Author URI:      https://newspack.blog
  * Text Domain:     newspack-popups
  * Domain Path:     /languages
- * Version:         1.28.0
+ * Version:         1.29.0
  *
  * @package         Newspack_Popups
  */
