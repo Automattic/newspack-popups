b6c3a35fce6b2e0fe3d10f6b5457f3b2e25617b6
Adam Borowski <adam@adamboro.com>
2021-07-16 10:53:18 +0200

    fix: assume DB prefix; ensure https

--- includes/class-newspack-popups-model.php
+++ includes/class-newspack-popups-model.php
@@ -1054,7 +1054,7 @@ final class Newspack_Popups_Model {
 	 * @return string Endpoint URL.
 	 */
 	public static function get_reader_endpoint() {
-		return plugins_url( '../api/campaigns/index.php', __FILE__ );
+		return str_replace( 'http:', 'https:', plugins_url( '../api/campaigns/index.php', __FILE__ ) );
 	}
 
 	/**
