25e34a51a07d228ea31ca97b63673615c2c352e5
Adam Borowski <adam@adamboro.com>
2021-07-22 08:53:10 +0200

    fix: update regex

--- includes/class-newspack-popups.php
+++ includes/class-newspack-popups.php
@@ -173,7 +173,7 @@ final class Newspack_Popups {
 				'single'            => true,
 				'auth_callback'     => '__return_true',
 				'sanitize_callback' => function( $input ) {
-					return preg_replace( '~[^-a-z0-9_]+~', '', $input );
+					return preg_replace( '~[^-\w0-9_\s]+~', '', $input );
 				},
 			]
 		);
