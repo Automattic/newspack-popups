5d5d539b61cb709e91da4f98ad75158df97acd4b
matticbot <semantic-release-bot@martynus.net>
2021-12-01 13:44:17 +0000

    chore(release): 1.45.1-alpha.1 [skip ci]
    ## [1.45.1-alpha.1](https://github.com/Automattic/newspack-popups/compare/v1.45.0...v1.45.1-alpha.1) (2021-12-01)
    
    ### Bug Fixes
    
    * path to config file ([2758ae1](https://github.com/Automattic/newspack-popups/commit/2758ae1f7354028628b82f6a1f44c2558cf92ab8))

--- newspack-popups.php
+++ newspack-popups.php
@@ -7,7 +7,7 @@
  * Author URI:      https://newspack.blog
  * Text Domain:     newspack-popups
  * Domain Path:     /languages
- * Version:         1.45.0
+ * Version:         1.45.1-alpha.1
  *
  * @package         Newspack_Popups
  */
