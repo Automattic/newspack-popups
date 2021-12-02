8827fbfd6939a7c54960fe453c5f6db2d1ea630d
Miguel Peixe <miguel@peixe.co>
2021-12-02 13:02:34 -0300

    test: implement block name assertion

--- tests/test-content-intertion.php
+++ tests/test-content-intertion.php
@@ -36,6 +36,18 @@ class ContentInsertionTest extends WP_UnitTestCase {
 	}
 
 	/**
+	 * Assert that that serialized blocks match the block names.
+	 *
+	 * @param string[] $expected List of block names.
+	 * @param array    $actual   Parsed blocks for assertion.
+	 * @param string   $message  Message.
+	 */
+	private static function assertEqualBlockNames( $expected, $actual, $message = '' ) {
+		$actual_names = wp_list_pluck( $actual, 'blockName' );
+		self::assertEquals( $expected, $actual_names, $message );
+	}
+
+	/**
 	 * Insertion into block-based post content.
 	 */
 	public function test_insertion_into_block_content() {
@@ -64,24 +76,26 @@ class ContentInsertionTest extends WP_UnitTestCase {
 			// A popup after all content.
 			self::create_inline_popup( '3', '100' ),
 		];
-		$content_with_popups = serialize_blocks(
-			Newspack_Popups_Inserter::insert_popups_in_post_content(
-				$post_content,
-				$popups
+		$content_with_popups = parse_blocks(
+			str_replace(
+				array( "\n", "\r" ),
+				'',
+				Newspack_Popups_Inserter::insert_popups_in_post_content(
+					$post_content,
+					$popups
+				)
 			)
 		);
-		self::assertEquals(
-			serialize_blocks(
-				self::rendered_popup( '1' ) . '<!-- wp:image {"align":"right"} -->
-<div class="wp-block-image">image</div>
-<!-- /wp:image --><!-- wp:paragraph -->
-<p>Paragraph 1</p>
-<!-- /wp:paragraph -->' . self::rendered_popup( '2' ) . '<!-- wp:heading -->
-<h2>A heading</h2>
-<!-- /wp:heading --><!-- wp:paragraph -->
-<p>Paragraph 2</p>
-<!-- /wp:paragraph -->' . self::rendered_popup( '3' )
-			),
+		self::assertEqualBlockNames(
+			[
+				'core/shortcode', // Popup 1.
+				'core/image',
+				'core/paragraph',
+				'core/shortcode', // Popup 2.
+				'core/heading',
+				'core/paragraph', // Popup 3.
+				'core/shortcode',
+			],
 			$content_with_popups,
 			'The popups are inserted into the content at expected positions.'
 		);
@@ -103,14 +117,22 @@ Paragraph 2
 			// A popup after all content.
 			self::create_inline_popup( '3', '100' ),
 		];
-		$content_with_popups = serialize_blocks(
+		$content_with_popups = parse_blocks(
 			Newspack_Popups_Inserter::insert_popups_in_post_content(
 				$post_content,
 				$popups
 			)
 		);
-		self::assertEquals(
-			serialize_blocks( self::rendered_popup( '1' ) . '<!-- wp:html --><p>Paragraph 1</p><!-- /wp:html -->' . self::rendered_popup( '2' ) . '<!-- wp:heading --><h2>A heading</h2><!-- /wp:heading --><!-- wp:html --><p>Paragraph 2</p><!-- /wp:html --><!-- wp:html --><blockquote><p>A quote</p></blockquote><!-- /wp:html -->' . self::rendered_popup( '3' ) ),
+		self::assertEqualBlockNames(
+			[
+				'core/shortcode', // Popup 1.
+				'core/html',
+				'core/shortcode', // Popup 2.
+				'core/heading',
+				'core/html',
+				'core/html',
+				'core/shortcode', // Popup 3.
+			],
 			$content_with_popups,
 			'The popups are inserted into the content at expected positions.'
 		);
