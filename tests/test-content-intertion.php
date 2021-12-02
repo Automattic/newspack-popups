bce629767fda5f425e5898253226a5a1415e7256
Miguel Peixe <miguel@peixe.co>
2021-12-02 12:15:24 -0300

    test: assert serialized blocks

--- tests/test-content-intertion.php
+++ tests/test-content-intertion.php
@@ -64,12 +64,15 @@ class ContentInsertionTest extends WP_UnitTestCase {
 			// A popup after all content.
 			self::create_inline_popup( '3', '100' ),
 		];
-		$content_with_popups = Newspack_Popups_Inserter::insert_popups_in_post_content(
-			$post_content,
-			$popups
+		$content_with_popups = serialize_blocks(
+			Newspack_Popups_Inserter::insert_popups_in_post_content(
+				$post_content,
+				$popups
+			)
 		);
 		self::assertEquals(
-			self::rendered_popup( '1' ) . '<!-- wp:image {"align":"right"} -->
+			serialize_blocks(
+				self::rendered_popup( '1' ) . '<!-- wp:image {"align":"right"} -->
 <div class="wp-block-image">image</div>
 <!-- /wp:image --><!-- wp:paragraph -->
 <p>Paragraph 1</p>
@@ -77,7 +80,8 @@ class ContentInsertionTest extends WP_UnitTestCase {
 <h2>A heading</h2>
 <!-- /wp:heading --><!-- wp:paragraph -->
 <p>Paragraph 2</p>
-<!-- /wp:paragraph -->' . self::rendered_popup( '3' ),
+<!-- /wp:paragraph -->' . self::rendered_popup( '3' )
+			),
 			$content_with_popups,
 			'The popups are inserted into the content at expected positions.'
 		);
@@ -99,12 +103,14 @@ Paragraph 2
 			// A popup after all content.
 			self::create_inline_popup( '3', '100' ),
 		];
-		$content_with_popups = Newspack_Popups_Inserter::insert_popups_in_post_content(
-			$post_content,
-			$popups
+		$content_with_popups = serialize_blocks(
+			Newspack_Popups_Inserter::insert_popups_in_post_content(
+				$post_content,
+				$popups
+			)
 		);
 		self::assertEquals(
-			self::rendered_popup( '1' ) . '<!-- wp:html --><p>Paragraph 1</p><!-- /wp:html -->' . self::rendered_popup( '2' ) . '<!-- wp:heading --><h2>A heading</h2><!-- /wp:heading --><!-- wp:html --><p>Paragraph 2</p><!-- /wp:html --><!-- wp:html --><blockquote><p>A quote</p></blockquote><!-- /wp:html -->' . self::rendered_popup( '3' ),
+			serialize_blocks( self::rendered_popup( '1' ) . '<!-- wp:html --><p>Paragraph 1</p><!-- /wp:html -->' . self::rendered_popup( '2' ) . '<!-- wp:heading --><h2>A heading</h2><!-- /wp:heading --><!-- wp:html --><p>Paragraph 2</p><!-- /wp:html --><!-- wp:html --><blockquote><p>A quote</p></blockquote><!-- /wp:html -->' . self::rendered_popup( '3' ) ),
 			$content_with_popups,
 			'The popups are inserted into the content at expected positions.'
 		);
