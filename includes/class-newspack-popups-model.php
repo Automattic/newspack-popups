c16e7d0b34ca8d6ae70ae2278fa6c8917c4f325d
Derrick Koo <derrick.koo@automattic.com>
2022-03-16 13:59:15 -0600

    fix: do not track analytics for logged-in editors and admins

--- includes/class-newspack-popups-model.php
+++ includes/class-newspack-popups-model.php
@@ -753,7 +753,8 @@ final class Newspack_Popups_Model {
 	protected static function insert_event_tracking( $popup, $body, $element_id ) {
 		if (
 			Newspack_Popups::is_preview_request() ||
-			Newspack_Popups_Settings::is_non_interactive()
+			Newspack_Popups_Settings::is_non_interactive() ||
+			! Newspack_Popups::is_tracking()
 		) {
 			return '';
 		}
@@ -848,7 +849,7 @@ final class Newspack_Popups_Model {
 	 * @param string $element_id The id of the popup element.
 	 */
 	private static function get_analytics_events( $popup, $body, $element_id ) {
-		if ( Newspack_Popups::is_preview_request() ) {
+		if ( Newspack_Popups::is_preview_request() || ! Newspack_Popups::is_tracking() ) {
 			return [];
 		}
 
