616ec53acb23e281164f0b0caab26378a63268e0
matticbot <semantic-release-bot@martynus.net>
2022-06-24 15:23:08 +0000

    chore(release): 2.0.1 [skip ci]
    ## [2.0.1](https://github.com/Automattic/newspack-popups/compare/v2.0.0...v2.0.1) (2022-06-24)
    
    ### Bug Fixes
    
    * misnamed variable in GA non-AMP config filter ([40ae65b](https://github.com/Automattic/newspack-popups/commit/40ae65b9a24c19b6f0efba4d45a3a2a774c20920))

--- newspack-popups.php
+++ newspack-popups.php
@@ -7,7 +7,7 @@
  * Author URI:      https://newspack.blog
  * Text Domain:     newspack-popups
  * Domain Path:     /languages
- * Version:         2.0.0
+ * Version:         2.0.1
  *
  * @package         Newspack_Popups
  */
