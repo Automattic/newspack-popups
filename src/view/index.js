b4c930cc8ca735f62c0f2cb4660e4da156dd75f8
Adam Borowski <adam@adamboro.com>
2022-09-09 14:51:59 +0200

    feat: remove wp-dom-ready, medialement dependencies for frontend script

--- src/view/index.js
+++ src/view/index.js
@@ -4,9 +4,26 @@
 import 'whatwg-fetch';
 
 /**
- * WordPress dependencies
+ * Specify a function to execute when the DOM is fully loaded.
+ *
+ * @see https://github.com/WordPress/gutenberg/blob/trunk/packages/dom-ready/
+ *
+ * @param {Function} callback A function to execute after the DOM is ready.
+ * @return {void}
  */
-import domReady from '@wordpress/dom-ready';
+function domReady( callback ) {
+	if ( typeof document === 'undefined' ) {
+		return;
+	}
+	if (
+		document.readyState === 'complete' || // DOMContentLoaded + Images/Styles/etc loaded, so we call directly.
+		document.readyState === 'interactive' // DOMContentLoaded fires at this point, so we call directly.
+	) {
+		return void callback();
+	}
+	// DOMContentLoaded has not fired yet, delay callback until then.
+	document.addEventListener( 'DOMContentLoaded', callback );
+}
 
 /**
  * Internal dependencies
