b4c41e2eee844c346beb3d219e7463abf5124aeb
Derrick Koo <derrick.koo@automattic.com>
2022-11-17 17:16:22 -0700

    feat: persist toggle selection in session

--- src/view/admin.js
+++ src/view/admin.js
@@ -4,10 +4,16 @@
 import './admin.scss';
 
 const toggle = document.querySelectorAll( '.newspack-campaigns-preview-toggle' );
+const isHidden = parseInt( localStorage.getItem( 'newspackPopupsHide' ) );
+
+if ( isHidden ) {
+	document.body.classList.add( 'newspack-popups-hide-prompts' );
+}
 
 const toggleHandler = e => {
 	e.preventDefault();
 	document.body.classList.toggle( 'newspack-popups-hide-prompts' );
+	localStorage.setItem( 'newspackPopupsHide', isHidden ? 0 : 1 );
 };
 
 for ( let i = 0; i < toggle.length; i++ ) {
