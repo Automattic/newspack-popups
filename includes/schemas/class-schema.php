b244f9e526cb3a18df6e6d5b69dbb7ca6224b7ee
leogermani <leogermani@automattic.com>
2022-12-01 16:07:57 -0300

    feat: create Json schemas (#1019)
    * Create Json schemas
    
    * Update tests/test-schemas.php
    
    Co-authored-by: Derrick Koo <dkoo@users.noreply.github.com>
    
    * Update tests/test-schemas.php
    
    Co-authored-by: Derrick Koo <dkoo@users.noreply.github.com>
    
    * dynamic overlay sizes
    
    * remove placeholder code
    
    * feat: update categories tags schema
    
    Co-authored-by: Derrick Koo <dkoo@users.noreply.github.com>

---
+++ includes/schemas/class-schema.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Abstract Schema class
+ *
+ * @package Newspack
+ */
+
+namespace Newspack\Campaigns;
+
+/**
+ * Abstract Schema class
+ */
+abstract class Schema {
+
+	/**
+	 * The input value to be handled.
+	 *
+	 * @var mixed
+	 */
+	protected $value;
+
+	/**
+	 * The validation errors found
+	 *
+	 * @var array
+	 */
+	protected $errors = [];
+
+	/**
+	 * Constructor.
+	 *
+	 * @param mixed $value The value to be validated.
+	 */
+	public function __construct( $value ) {
+		$this->value = $value;
+	}
+
+	/**
+	 * Gets the Schema. This method must be overridden by the extending class.
+	 *
+	 * @return array The Schema.
+	 */
+	abstract public function get_schema();
+
+	/**
+	 * Validate the value against the schema.
+	 *
+	 * @return boolean
+	 */
+	public function is_valid() {
+		$result = rest_validate_value_from_schema( $this->value, $this->get_schema() );
+		if ( is_wp_error( $result ) ) {
+			$this->errors = $result->get_error_messages();
+			return false;
+		}
+		$this->errors = array();
+		return true;
+	}
+
+	/**
+	 * Get the validation errors.
+	 *
+	 * @return array
+	 */
+	public function get_errors() {
+		return $this->errors;
+	}
+}
