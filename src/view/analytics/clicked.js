e072082fd99f5e9f2b1fc2c2b0308e7e0546b8e3
Laurel Fulford <laurel.fulford@automattic.com>
2023-06-07 12:54:32 -0700

    feat: add tracking for click events

---
+++ src/view/analytics/clicked.js
@@ -0,0 +1,23 @@
+import { getEventPayload, getRawId, sendEvent } from '../utils';
+
+/**
+ * Execute a callback function to send a GA event when a prompt is dismissed.
+ *
+ * @param {Array} prompts Array of prompts loaded in the DOM.
+ */
+
+export const manageClickedEvents = prompts => {
+	prompts.forEach( prompt => {
+		const anchorLinks = [
+			...prompt.querySelectorAll( '.newspack-inline-popup a, .newspack-popup__content a' ),
+		];
+		const handleEvent = () => {
+			const payload = getEventPayload( 'clicked', getRawId( prompt.getAttribute( 'id' ) ) );
+			sendEvent( payload );
+		};
+
+		anchorLinks.forEach( function ( link ) {
+			link.addEventListener( 'click', handleEvent );
+		} );
+	} );
+};
