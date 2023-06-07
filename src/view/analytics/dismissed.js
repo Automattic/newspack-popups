a66c142426c59923c627f6a39d8bb6f8842766b4
Laurel Fulford <laurel.fulford@automattic.com>
2023-06-07 11:33:37 -0700

    feat: simplifying function used to track clicks

--- src/view/analytics/dismissed.js
+++ src/view/analytics/dismissed.js
@@ -1,28 +1,19 @@
-import { getEventPayload, getRawId, parseOnHandlers } from '../utils';
+import { getEventPayload, getRawId } from '../utils';
 
 /**
  * Execute a callback function to send a GA event when a prompt is dismissed.
  *
- * @param {Function} handleEvent Callback function to execute when the prompt is dismissed.
- * @return
+ * @param {Array} prompts Array of prompts loaded in the DOM.
  */
 
-const manageBind = bindElement => {
-	const onHandlers = parseOnHandlers( bindElement.getAttribute( 'on' ) );
-	onHandlers.forEach( onHandler => {
-		if ( onHandler.action === 'tap' ) {
-			const handleClick = () => {
-				if ( onHandler.method === 'hide' ) {
-					const payload = getEventPayload( 'dismissed', getRawId( onHandler.id ) );
-					console.log( 'From dismiss: event name', 'prompt_interaction' );
-					console.log( 'From dismiss: event payload', payload );
-				}
-			};
-			bindElement.addEventListener( 'click', handleClick );
-		}
-	} );
-};
+export const manageDismissals = prompts => {
+	prompts.forEach( prompt => {
+		const closeButton = prompt.querySelector( '.newspack-lightbox__close' );
+		const payload = getEventPayload( 'dismiss', getRawId( prompt.getAttribute( 'id' ) ) );
+		const handleEvent = () => {
+			console.log( payload );
+		};
 
-export const manageDismissals = () => {
-	[ ...document.querySelectorAll( '.newspack-popup button[on]' ) ].map( manageBind );
+		closeButton.addEventListener( 'click', handleEvent );
+	} );
 };
