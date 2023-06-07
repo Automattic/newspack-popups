89d5a3c8252e3740c6189d78bbe6b61f060e6c78
Derrick Koo <derrick.koo@automattic.com>
2023-06-07 15:14:59 -0600

    feat: allow action_value param to store browser interaction data in payload

--- src/view/analytics/clicked.js
+++ src/view/analytics/clicked.js
@@ -11,8 +11,18 @@ export const manageClickedEvents = prompts => {
 		const anchorLinks = [
 			...prompt.querySelectorAll( '.newspack-inline-popup a, .newspack-popup__content a' ),
 		];
-		const handleEvent = () => {
-			const payload = getEventPayload( 'clicked', getRawId( prompt.getAttribute( 'id' ) ) );
+		const handleEvent = e => {
+			const extraParams = {};
+
+			if ( e.currentTarget?.href && '#' !== e.currentTarget?.href ) {
+				extraParams.action_value = e.currentTarget.getAttribute( 'href' );
+			}
+
+			const payload = getEventPayload(
+				'clicked',
+				getRawId( prompt.getAttribute( 'id' ) ),
+				extraParams
+			);
 			sendEvent( payload );
 		};
 
