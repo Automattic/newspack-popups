5c191d72df88390185823d68274d47e51a3277d5
philipjohn <philip.john@automattic.com>
2023-07-12 15:51:41 +0100

    Fix the use of wp_terms() without checking it's an array (#1146)
    * Fix the use of wp_terms() without checking it's an array
    
    * Refine docblock
    
    Co-authored-by: leogermani <leogermani@gmail.com>
    
    ---------
    
    Co-authored-by: leogermani <leogermani@gmail.com>

--- includes/class-newspack-popups.php
+++ includes/class-newspack-popups.php
@@ -1164,8 +1164,20 @@ final class Newspack_Popups {
 
 	/**
 	 * Retrieve campaigns.
+	 *
+	 * @return WP_Term[] An array of WP_Term objects.
 	 */
 	public static function get_groups() {
+		$terms = get_terms(
+			self::NEWSPACK_POPUPS_TAXONOMY,
+			[
+				'hide_empty' => false,
+			]
+		);
+		if ( ! is_array( $terms ) || empty( $terms ) ) {
+			return [];
+		}
+
 		$groups = array_map(
 			function( $group ) {
 				$group->status = get_term_meta(
@@ -1175,12 +1187,7 @@ final class Newspack_Popups {
 				);
 				return $group;
 			},
-			get_terms(
-				self::NEWSPACK_POPUPS_TAXONOMY,
-				[
-					'hide_empty' => false,
-				]
-			)
+			$terms
 		);
 		return $groups;
 	}
