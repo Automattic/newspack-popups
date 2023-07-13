06c82488c5b5e5d1599d0c849db9b78798b5846b
Derrick Koo <derrick.koo@automattic.com>
2023-07-13 17:54:59 -0600

    test: remove tests using deprecated features

--- tests/test-e2e.php
+++ tests/test-e2e.php
@@ -11,23 +11,6 @@
  */
 class E2ETest extends WP_UnitTestCase_PageWithPopups {
 	/**
-	 * Basic E2E test - render popups on a page and check API response.
-	 */
-	public function test_e2e_basic() {
-		$another_popup_id = self::createPopup();
-		self::renderPost();
-
-		self::assertEquals(
-			[
-				'id_' . $another_popup_id => true,
-				'id_' . self::$popup_id   => true,
-			],
-			self::getAPIResponse(),
-			'API response has expected shape.'
-		);
-	}
-
-	/**
 	 * Mutual exclusion.
 	 * A page should render at most one popup of each condition:
 	 * - overlays,
@@ -69,19 +52,6 @@ class E2ETest extends WP_UnitTestCase_PageWithPopups {
 		];
 
 		self::renderPost( '', '<!-- wp:newspack-popups/custom-placement {"customPlacement":"custom1"} /-->' );
-
-		self::assertEquals(
-			[
-				'id_' . $popups[0] => false,
-				'id_' . $popups[1] => true,
-				'id_' . $popups[2] => false,
-				'id_' . $popups[3] => true,
-				'id_' . $popups[4] => false,
-				'id_' . $popups[5] => true,
-			],
-			self::getAPIResponse(),
-			'Only one popup (the one with segment assigned) of each mutually-excluding pair is displayed.'
-		);
 	}
 
 	/**
