418cef9c40e895531a9e0051994a3c4fdbbf33b4
Derrick Koo <dkoo@users.noreply.github.com>
2023-07-28 14:59:30 -0600

    feat: favorite categories segmentation logic (#1177)
    * feat: logic for favorite categories
    
    * fix: top-ranked category must actually have more views than the other categories
    
    * fix: simplify logic and account for undefined countsArray
    
    * fix: account for multiple views in one category; simplify logic a bit
    
    * fix: correct indexOf comparison

--- src/criteria/default/favorite-categories.js
+++ src/criteria/default/favorite-categories.js
@@ -1,7 +1,13 @@
 import { setMatchingFunction } from '../utils';
 
 setMatchingFunction( 'favorite_categories', ( config, ras ) => {
+	let match = false;
 	const views = ras.getUniqueActivitiesBy( 'article_view', 'post_id' );
+
+	if ( 1 >= views.length ) {
+		return match;
+	}
+
 	const categories = views.reduce( ( c, v ) => {
 		if ( v.data?.categories?.length ) {
 			c.push( ...v.data.categories );
@@ -14,6 +20,17 @@ setMatchingFunction( 'favorite_categories', ( config, ras ) => {
 	}, {} );
 	const countsArray = Object.entries( counts );
 	countsArray.sort( ( a, b ) => b[ 1 ] - a[ 1 ] );
-	/* TODO: Decide how to rank categories. */
-	return false;
+
+	if ( ! countsArray || ! countsArray.length ) {
+		return match;
+	}
+
+	// Must have viewed at least 2 categories or 2 posts within the same category in order to rank.
+	if ( ! countsArray[ 1 ] || countsArray[ 0 ][ 1 ] > countsArray[ 1 ][ 1 ] ) {
+		if ( -1 < config.value.indexOf( parseInt( countsArray[ 0 ][ 0 ] ) ) ) {
+			match = true;
+		}
+	}
+
+	return match;
 } );
