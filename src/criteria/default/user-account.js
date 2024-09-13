ca64f9815927c476c4100a8e04f8b5a953355b20
Miguel Peixe <miguel.peixe@a8c.com>
2024-09-13 16:26:25 -0300

    fix: check for non-preview logged in user on account criteria

--- src/criteria/default/user-account.js
+++ src/criteria/default/user-account.js
@@ -1,10 +1,11 @@
+/* globals newspackPopupsCriteria */
 import { setMatchingFunction } from '../utils';
 
 setMatchingFunction( 'user_account', ( config, { store } ) => {
 	switch ( config.value ) {
 		case 'with-account':
-			return store.get( 'reader' )?.email;
+			return newspackPopupsCriteria.is_non_preview_user || store.get( 'reader' )?.email;
 		case 'without-account':
-			return ! store.get( 'reader' )?.email;
+			return ! newspackPopupsCriteria.is_non_preview_user && ! store.get( 'reader' )?.email;
 	}
 } );
