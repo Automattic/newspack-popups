b1db534fbab7031fbe5a39389c9eefec2558778b
Rasmy Nguyen <raz@automattic.com>
2024-11-04 17:05:52 -0500

    fix(segmentation): account for unauthenticated ras state

--- src/criteria/default/user-account.js
+++ src/criteria/default/user-account.js
@@ -2,10 +2,11 @@
 import { setMatchingFunction } from '../utils';
 
 setMatchingFunction( 'user_account', ( config, { store } ) => {
+	const reader = store?.get?.( 'reader' );
 	switch ( config.value ) {
 		case 'with-account':
-			return newspackPopupsCriteria.is_non_preview_user || store.get( 'reader' )?.email;
+			return newspackPopupsCriteria.is_non_preview_user || ( reader?.email && reader?.authenticated );
 		case 'without-account':
-			return ! newspackPopupsCriteria.is_non_preview_user && ! store.get( 'reader' )?.email;
+			return ! newspackPopupsCriteria.is_non_preview_user && ( ! reader?.email || ! reader?.authenticated );
 	}
 } );
