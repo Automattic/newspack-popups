6ad1eb9c934fcb8d2a9922b02e7637c787db3f96
Laurel Fulford <laurel.fulford@automattic.com>
2025-01-20 11:23:45 -0800

    fix: simplify no-padding styles

--- includes/class-newspack-popups-model.php
+++ includes/class-newspack-popups-model.php
@@ -1044,8 +1044,7 @@ final class Newspack_Popups_Model {
 		$overlay_color         = $popup['options']['overlay_color'];
 		$overlay_size          = 'full' === $popup['options']['overlay_size'] ? 'full-width' : $popup['options']['overlay_size'];
 		$no_overlay_background = $popup['options']['no_overlay_background'];
-		$close_button_color    = ( ! $no_overlay_background && $no_padding ) ? self::foreground_color_for_background( $popup['options']['overlay_color'] ) : '#000';
-		$close_button_shadow   = ( ! $no_overlay_background && $no_padding ) ? $overlay_color . 'aa' : 'transparent';
+		$close_button_color    = ( ! $no_overlay_background && $no_padding ) ? self::foreground_color_for_background( $popup['options']['background_color'] ) : '#000';
 		$hidden_fields         = self::get_hidden_fields( $popup );
 		$is_newsletter_prompt  = self::has_newsletter_prompt( $popup );
 		$has_featured_image    = has_post_thumbnail( $popup['id'] ) || ! empty( $popup['options']['featured_image_id'] );
@@ -1082,7 +1081,8 @@ final class Newspack_Popups_Model {
 			data-delay="<?php echo esc_attr( self::get_delay( $popup ) ); ?>"
 			<?php endif; ?>
 		>
-			<div class="<?php echo esc_attr( implode( ' ', $wrapper_classes ) ); ?>" data-popup-status="<?php echo esc_attr( $popup['status'] ); ?>" style="<?php echo ! $hide_border && ! $no_padding ? esc_attr( self::container_style( $popup ) ) : ''; ?>">
+
+			<div class="<?php echo esc_attr( implode( ' ', $wrapper_classes ) ); ?>" data-popup-status="<?php echo esc_attr( $popup['status'] ); ?>" style="<?php echo ! ( $hide_border && ! self::is_overlay( $popup ) ) ? esc_attr( self::container_style( $popup ) ) : ''; ?>">
 				<div class="newspack-popup__content-wrapper" style="<?php echo $hide_border || $no_padding ? esc_attr( self::container_style( $popup ) ) : ''; ?>">
 					<?php if ( $has_featured_image ) : ?>
 						<div class="newspack-popup__featured-image">
@@ -1094,7 +1094,7 @@ final class Newspack_Popups_Model {
 					</div>
 					<button class="newspack-lightbox__close" style="color: <?php echo esc_attr( $close_button_color ); ?>"aria-label="<?php esc_html_e( 'Close Pop-up', 'newspack-popups' ); // phpcs:ignore WordPressVIPMinimum.Security.ProperEscapingFunction.htmlAttrNotByEscHTML ?>">
 						<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" role="img" aria-hidden="true" focusable="false">
-							<path style="filter: drop-shadow(0 0 3px <?php echo esc_attr( $close_button_shadow ); ?>); " d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/>
+							<path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/>
 						</svg>
 					</button>
 				</div>
