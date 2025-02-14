23b3d8f42ce870dd3db30e8537a152ae7704b489
Laurel Fulford <laurel.fulford@automattic.com>
2025-02-14 15:57:18 -0800

    fix: trying to pass schema tests

--- tests/test-schemas.php
+++ tests/test-schemas.php
@@ -42,7 +42,7 @@ class SchemasTest extends WP_UnitTestCase {
 					'duplicate_of'      => 0,
 					'options'           => [
 						'background_color'               => '#FFFFFF',
-						'close_button_background_color'  => '',
+						'close_button_background_color'  => '#FFFFFF',
 						'hide_border'                    => false,
 						'large_border'                   => false,
 						'no_padding'                     => false,
