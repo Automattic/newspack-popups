f22337b0aae6819fadce467c7f08f0ceb3febfc0
Camilla Krag Jensen <naxoc@users.noreply.github.com>
2025-02-18 15:31:50 +0100

    fix(dev-env): remove test env const check

--- includes/class-newspack-popups-inserter.php
+++ includes/class-newspack-popups-inserter.php
@@ -619,10 +619,6 @@ final class Newspack_Popups_Inserter {
 	 * Enqueue the assets needed to display the popups.
 	 */
 	public static function enqueue_scripts() {
-		if ( defined( 'IS_TEST_ENV' ) && IS_TEST_ENV ) {
-			return;
-		}
-
 		// Don't enqueue assets if prompts are disabled on this post.
 		$has_disabled_prompts = is_singular() && ! empty( get_post_meta( get_the_ID(), 'newspack_popups_has_disabled_popups', true ) );
 		if ( $has_disabled_prompts ) {
