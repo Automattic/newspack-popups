83486d87753b42343b34f7320dfc97792af5372e
Camilla Krag Jensen <naxoc@users.noreply.github.com>
2025-06-03 12:07:26 +0200

    fix: correct URL for segments admin link

--- includes/class-newspack-popups.php
+++ includes/class-newspack-popups.php
@@ -765,7 +765,7 @@ final class Newspack_Popups {
 				'taxonomy'                     => self::NEWSPACK_POPUPS_TAXONOMY,
 				'is_prompt'                    => self::NEWSPACK_POPUPS_CPT == get_post_type(),
 				'segments_taxonomy'            => Newspack_Segments_Model::TAX_SLUG,
-				'segments_admin_url'           => admin_url( 'admin.php?page=newspack-audience-campaigns#/campaigns' ),
+				'segments_admin_url'           => admin_url( 'admin.php?page=newspack-audience-campaigns#/segments' ),
 				'available_post_types'         => array_values(
 					get_post_types(
 						[
