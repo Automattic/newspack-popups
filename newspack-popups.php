797596175f1ef948f867f2d3e668306c4ca0e92f
matticbot <semantic-release-bot@martynus.net>
2025-11-03 14:08:55 +0000

    chore(release): 3.7.1-alpha.1 [skip ci]
    ## [3.7.1-alpha.1](https://github.com/Automattic/newspack-popups/compare/v3.7.0...v3.7.1-alpha.1) (2025-11-03)
    
    ### Bug Fixes
    
    * revert [#1485](https://github.com/Automattic/newspack-popups/issues/1485) ([#1487](https://github.com/Automattic/newspack-popups/issues/1487)) ([e811f4f](https://github.com/Automattic/newspack-popups/commit/e811f4fdb373edb4c106756e23fc0e6431a1fa81))
    * update metering class namespace ([#1486](https://github.com/Automattic/newspack-popups/issues/1486)) ([8e13e05](https://github.com/Automattic/newspack-popups/commit/8e13e0506601608e8978a97921f9e3441ebe4c3c))

--- newspack-popups.php
+++ newspack-popups.php
@@ -7,7 +7,7 @@
  * Author URI:      https://newspack.com
  * Text Domain:     newspack-popups
  * Domain Path:     /languages
- * Version:         3.7.0
+ * Version:         3.7.1-alpha.1
  *
  * @package         Newspack_Popups
  */
