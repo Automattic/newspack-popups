36f814b1487be6cd2148293ad15f38af39fb6778
Laurel Fulford <laurel.fulford@automattic.com>
2026-01-05 15:05:34 -0800

    fix: remove form_submitted action from np_prompt_interaction

--- src/view/analytics/ga4.js
+++ src/view/analytics/ga4.js
@@ -4,7 +4,6 @@ import { manageLoadedEvents } from './loaded';
 import { manageSeenEvents } from './seen';
 import { manageDismissals } from './dismissed';
 import { manageClickedEvents } from './clicked';
-import { manageFormSubmissions } from './submitted';
 
 export const handleAnalytics = prompts => {
 	// Must have a gtag instance to proceed.
@@ -13,6 +12,5 @@ export const handleAnalytics = prompts => {
 		manageSeenEvents();
 		manageDismissals( prompts );
 		manageClickedEvents( prompts );
-		manageFormSubmissions( prompts );
 	}
 };
