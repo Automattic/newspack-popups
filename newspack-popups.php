942be434d195aefc8ed10ad7fd2f7b23e153b363
matticbot <semantic-release-bot@martynus.net>
2026-01-12 15:15:53 +0000

    chore(release): 3.8.3 [skip ci]
    ## [3.8.3](https://github.com/Automattic/newspack-popups/compare/v3.8.2...v3.8.3) (2026-01-12)
    
    ### Bug Fixes
    
    * ignore corrections when validating content for inline prompts ([#1511](https://github.com/Automattic/newspack-popups/issues/1511)) ([034ebed](https://github.com/Automattic/newspack-popups/commit/034ebed8e4cee5402aa10f2a3097ad57df0b4ac4))

--- newspack-popups.php
+++ newspack-popups.php
@@ -7,7 +7,7 @@
  * Author URI:      https://newspack.com
  * Text Domain:     newspack-popups
  * Domain Path:     /languages
- * Version:         3.8.2
+ * Version:         3.8.3
  *
  * @package         Newspack_Popups
  */
