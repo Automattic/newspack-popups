ea29a749a3047ddafb4edb03586d89190508c8ff
Laurel Fulford <laurel.fulford@automattic.com>
2026-02-06 15:24:45 -0800

    fix: add a check for block theme and run do_shortcode on them

--- src/blocks/single-prompt/view.php
+++ src/blocks/single-prompt/view.php
@@ -35,6 +35,7 @@ function render_block( $attributes ) {
 	$content     = '';
 	$prompt_id   = $attributes['promptId'];
 	$class_names = isset( $attributes['className'] ) ? ' class="' . $attributes['className'] . '"' : '';
+	$is_block_theme = function_exists( 'wp_is_block_theme' ) && wp_is_block_theme();
 
 	if ( empty( $prompt_id ) ) {
 		return $content;
@@ -49,7 +50,10 @@ function render_block( $attributes ) {
 			$content .= '<!-- Newspack Campaigns: Start Prompt ' . $prompt_id . '-->';
 		}
 
-		$content .= '<!-- wp:shortcode -->[newspack-popup id="' . $prompt_id . '"' . $class_names . ']<!-- /wp:shortcode -->';
+		$shortcode = '[newspack-popup id="' . $prompt_id . '"' . $class_names . ']';
+		$content  .= $is_block_theme
+			? do_shortcode( $shortcode )
+			: '<!-- wp:shortcode -->' . $shortcode . '<!-- /wp:shortcode -->';
 
 		if ( defined( 'WP_NEWSPACK_DEBUG' ) && WP_NEWSPACK_DEBUG ) {
 			$content .= '<!-- Newspack Campaigns: End Prompt ' . $prompt_id . '-->';
